78e6531350c5be0ec484c516c65d236cea6ac2c3
Ravindra7330 <ravindarravi87@gmail.com>
2025-09-09 15:49:42 +0530

    room logic

--- websocket-frontend/src/App.tsx
+++ websocket-frontend/src/App.tsx
@@ -5,30 +5,49 @@ import { BACKEND_URL } from './config';
 function App() {
   const [messages,setMessages]=useState([])
   const [input, setInput] = useState('');
+  const [roomId, setRoomId] = useState('');
+  const [isJoined, setIsJoined] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
  const wsRef = useRef<WebSocket | null>(null);
   useEffect(()=>{
-    const ws = new WebSocket(BACKEND_URL);
+    const ws = new WebSocket('ws://localhost:10000');
     ws.onmessage=(event)=>{
       //@ts-ignore
       setMessages(m=>[...m, event.data])
     }
     wsRef.current = ws;
     ws.onopen=()=>{
-      ws.send(JSON.stringify({
-        type:"join",
-        payload:{
-          roomId:"red"
-        }
-      }))
+      
     }
     return ()=>{
       ws.close()
     }
   },[])
+
+    const joinRoom = () => {
+    if (roomId.trim() && wsRef.current?.readyState === WebSocket.OPEN) {
+      wsRef.current.send(JSON.stringify({ type: 'join', payload: { roomId } }));
+      setIsJoined(true);
+    }
+  };
   return (
     <div className="flex flex-col h-screen bg-gray-900">
+      {!isJoined ? (<div className="flex flex-col items-center justify-center flex-1 space-y-4">
+          <input
+            className="px-4 py-2 rounded-full bg-white outline-none"
+            placeholder="Enter Room ID"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+          />
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-800"
+            onClick={joinRoom}
+          >
+            Join Room
+          </button>
+        </div>)
+      :(<div className="flex flex-col h-screen bg-gray-900">
       <div className="flex-1 overflow-y-auto p-4 space-y-2">
         {messages.map(message => <div className="flex-1 overflow-y-auto p-4 space-y-2 bg-blue-500 text-white rounded-full pt-3 max-w-fit"> 
           {message}
@@ -64,7 +83,9 @@ function App() {
         </button>
       </form>
     
+    </div>)}
     </div>
+    
   );
 }
 
